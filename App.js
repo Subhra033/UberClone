bf52c36a3102aa615b041c33e413344d1958882a
Subhrajyoti Koley <koley.subhrajyoti@gmail.com>
2021-08-21 00:54:20 +0530

    store setup

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Lets Build Uber!</Text>
         <StatusBar style="auto" />
