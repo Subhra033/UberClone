06c0a6c2d5f4b814e5ec56e8455a2e223a7a796a
Subhrajyoti Koley <koley.subhrajyoti@gmail.com>
2021-08-28 17:34:31 +0530

    MAPS PI UDPATED

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Image, SafeAreaView, StyleSheet, View } from 'react-native'
 import tw from "tailwind-react-native-classnames";
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+
+
 
 //rnfes--Shortcut for React Native Functional component with Stylesheet
 //https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png
@@ -20,7 +24,25 @@ const HomeScreen = () => {
                         uri: "https://links.papareact.com/gzs",
                     }}
                 />
+                <GooglePlacesAutocomplete
+                    placeholder="Where From?"
+                    style={{
+                        container: {
+                            flex: 0,
+                        },
+                        textinput: {
+                            fontSize: 18,
+                        },
+                    }}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: "en",
+                    }
 
+                    }
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                />
                 <NavOptions />
             </View>
         </SafeAreaView>
